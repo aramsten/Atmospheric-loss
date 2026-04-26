b477acea1bf83a87a6439d61a5ce4fac938c5418
A Ramsten <ramstenandre@gmail.com>
2026-04-26 18:02:00 +0200

    Created a new script exoplanet_comparison_table_creator.py that create a ecsv file with duplicates for planets using different t_sat, lxuv/lsat and gamma.
    Created a new script exoplanet_comparison_study.py that used the created comparison-table to plot our baseline estimates as points, and estimates using other values for t_sat, lxuv/lsat and gamma.
    The other values are plotted with error bars, corresponding to the maximum and minimum possible values using the other authors values.
    
    Plot and ecsv uploaded.

---
+++ exoplanet_comparison_study.py
@@ -0,0 +1,217 @@
+from astropy.constants import L_sun, M_earth, R_earth, G
+import matplotlib.pyplot as plt
+from Plot_tools.plot_creator import save_plot
+import numpy as np
+from astropy.io import ascii
+from astropy import units as u
+from Calculators import function_solvers as fs
+
+
+def atmosphere_color(name):
+    """
+    Returns color based on atmospheric status of known planets
+    green <==> thick
+    orange <==> thin
+    red <==> airless
+    gray <==> unknown
+    """
+    name = name.replace('"', '')
+
+    # Solar system
+    if name == "Earth": return "green"
+    if name == "Venus": return "green"
+    if name == "Mars": return "orange"
+    if name == "Mercury": return "red"
+
+    # TRAPPIST-1
+    if name == "TRAPPIST-1 b": return "red"
+    if name == "TRAPPIST-1 c": return "red"
+    if name == "TRAPPIST-1 d": return "red"
+
+    return "gray"
+
+
+def plot_cosmic_shoreline_models(catalog, colname, x_label, y_label, eta, model_name, star, star_shortname):
+    base = catalog[catalog["variation_id"] == "baseline"]
+    model = catalog[catalog["model_id"] == model_name]
+
+    earth_idx = np.where(base["pl_name"] == "Earth")[0][0]
+    v_earth = base["v_esc"].quantity[earth_idx]
+
+    mass_base = base["pl_masse"].to(u.kg)
+    rad_base  = base["pl_rade"].to(u.m)
+    rho_base  = 3 * mass_base / (4 * np.pi * rad_base**3)
+    rho_earth = rho_base[earth_idx]
+
+    x_base = ((base["v_esc"] / v_earth)**3 * (rho_base / rho_earth)**0.5).decompose()
+    y_base = base[colname]
+
+    fig, ax = plt.subplots()
+
+    # Baseline
+    for i, name in enumerate(base["pl_name"]):
+        color = atmosphere_color(name)
+        ax.scatter(x_base.value[i], y_base[i],
+                   color=color, marker="o", s=40, zorder=3)
+
+        ax.text(x_base.value[i] * 1.05,
+                y_base[i] * 0.8,
+                short_label(name, star, star_shortname),
+                fontsize=7,
+                color="black",
+                bbox=dict(facecolor='white', edgecolor='none', alpha=0.8, pad=0))
+
+    # Compared model
+    for name in base["pl_name"]:
+        rows = model[model["pl_name"] == name]
+        if len(rows) == 0:
+            continue
+
+        y_vals = np.array([r[colname] for r in rows])
+        y_min, y_max = np.min(y_vals), np.max(y_vals)
+
+        nom_mask = rows["variation_id"] == "nom"
+        nom = rows[nom_mask][0] if np.any(nom_mask) else rows[0]
+        y_nom = nom[colname]
+
+        v_nom = nom["v_esc"]* catalog["v_esc"].unit
+        rho_nom = 3 * (nom["pl_masse"] * M_earth) / (4 * np.pi * (nom["pl_rade"] * R_earth)**3)
+        x_nom = ((v_nom / v_earth)**3 * (rho_nom / rho_earth)**0.5).decompose()
+
+        color = atmosphere_color(name)
+
+        ax.errorbar(
+            x_nom.value, y_nom,
+            yerr=[[y_nom - y_min], [y_max - y_nom]],
+            fmt='s',
+            color=color,
+            markersize=6,
+            capsize=3,
+            zorder=4
+        )
+
+        ax.text(x_nom.value * 1.05,
+                y_nom * 1.05,
+                short_label(name, star, star_shortname),
+                fontsize=7,
+                color="black",
+                bbox=dict(facecolor='white', edgecolor='none', alpha=0.8, pad=0.5))
+
+    # Axis settings
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+
+    ax.grid(True, which="both", linestyle="--", linewidth=0.6, alpha=0.7)
+
+    plot_margin = 0.07
+    x_min, x_max = ax.get_xlim()
+    y_min, y_max = ax.get_ylim()
+    ax.set_xlim(x_min * (1-plot_margin) , x_max * (1+plot_margin))
+    ax.set_ylim(y_min * (1-plot_margin), y_max * (1+plot_margin))
+
+    # Mass loss lines
+    mass_fractions = [2e-4, 2e-3, 1e-2, 2e-1]
+    draw_mass_loss_lines_for_comparison(ax, base, eta, x_base, y_base, mass_fractions, earth_idx)
+
+    # Legends
+    add_legends(ax, model_name)
+
+    return fig
+
+def draw_mass_loss_lines_for_comparison(ax, catalog, eta, x_axis, y_axis, fractions, earth_idx):
+    d = catalog["pl_orbsmax"].to(u.m)
+    lum = catalog["st_lum"]*L_sun
+    lq = catalog["Lxuv/Lbol"]
+    t_sat = catalog["t_sat"].to(u.s)
+    age = catalog["st_age"].to(u.s)
+    gamma = catalog["gamma"]
+
+    d_earth = d[earth_idx]
+    lum_sun = lum[earth_idx]
+    lq_sun = lq[earth_idx]
+    t_sat_sun = t_sat[earth_idx]
+    age_sun = age[earth_idx]
+    gamma_sun = gamma[earth_idx]
+
+    E_xuv_sun = fs.calculate_total_l_xuv(lum_sun, lq_sun, t_sat_sun, age_sun, gamma_sun)
+
+    v_esc = catalog["v_esc"].to(u.m/u.s)
+    rho = 3*catalog["pl_masse"].to(u.kg)/(4*np.pi*(catalog["pl_rade"].to(u.m))**3)
+    v_earth = v_esc[earth_idx]
+    rho_earth = rho[earth_idx]
+
+    xmin, xmax = ax.get_xlim()
+    x_vals = np.logspace(np.log10(xmin), np.log10(xmax), 200)
+
+    ax = plt.gca()
+
+    for f in fractions:
+        m = (d_earth ** 2 / (eta * E_xuv_sun)) * (8 * np.pi / (3 * G))**0.5 * f *(v_earth) ** 3 * (rho_earth)**0.5
+        y_line = m * x_vals
+        ax.plot(x_vals, y_line, linestyle="--", color="blue", alpha=0.3, zorder=1)
+        x_pos = x_vals[15] if f != 0.0002 else x_vals[80]
+        y_pos = y_line[15] if f != 0.0002 else y_line[82]
+        ax.text(x_pos, y_pos, rf"$\Delta M = {f}$",
+                fontsize=8,
+                va="center",
+                ha="center",
+                rotation =18,
+                zorder=2,
+                bbox=dict(facecolor='white', edgecolor='none', alpha=0.8, pad=1)
+                )
+
+def add_legends(ax, model_name):
+    # Legend 1: Marker shape (baseline vs model)
+    baseline_proxy = plt.Line2D([], [], marker="o", color="black", linestyle="None", label="Baseline")
+    model_proxy = plt.Line2D([], [], marker="s", color="black", linestyle="None", label=f"{model_name}")
+    legend1 = ax.legend(handles=[baseline_proxy, model_proxy],loc="lower left", fontsize=8, frameon=True)
+    ax.add_artist(legend1)
+
+    # Legend 2: Colour (atmospheric status)
+    atm_green = plt.Line2D([], [], marker="o", color="green", linestyle="None", label="Thick atmosphere")
+    atm_orange = plt.Line2D([], [], marker="o", color="orange",linestyle="None", label="Thin atmosphere")
+    atm_red = plt.Line2D([], [], marker="o", color="red", linestyle="None", label="Airless")
+    atm_gray = plt.Line2D([], [], marker="o", color="gray", linestyle="None", label="Unknown")
+    ax.legend(handles=[atm_green, atm_orange, atm_red, atm_gray], loc="upper left", fontsize=8, frameon=True)
+
+
+def short_label(name, star, star_shortname):
+    """
+    Returnerar kortare label för plot, ändrar inte catalog
+    exempelvis blir TRAPPIST-1 e omvandlat till T1-e
+    """
+    if name.startswith(f"{star} "):
+        suffix = name.split(f"{star} ")[1]
+        return f"{star_shortname}-{suffix}"
+    else:
+        return name
+
+def main():
+    initials = "AR"
+
+    table_name = "260426_10.17_AR_Catalog_comparison_TRAPPIST-1_for_0.1-10.0_Gyr_eta-0.1_Rxuv-1.0.ecsv"
+    catalog = ascii.read(f"Tables/{table_name}")
+    eta = catalog.meta["eta"]
+    R_xuv = catalog.meta["R_xuv"]
+    model_name = catalog.meta["Model"]
+    star = catalog.meta["Compared star"]
+    star_shortname = catalog.meta["Compared star shortname"]
+
+    plot_name = f"cosmic_shoreline_comparison_TRAPPIST-1_at_stars_age_lost-rxuv={R_xuv}-eta={eta}"
+
+    plot = plot_cosmic_shoreline_models(
+        catalog,
+        "insol_star_age",
+        r"$x = (v_\mathrm{esc}/v_\mathrm{esc\oplus})^3 (\rho / \rho_\oplus)^{0.5}$",
+        "Insolation relative to Earth",
+        eta,
+        model_name,
+        star,
+        star_shortname
+    )
+    save_plot(plot, initials, plot_name)
+
+if __name__ == "__main__":
+    main()
---
+++ exoplanet_comparison_table_creator.py
@@ -0,0 +1,86 @@
+from Data_management import nasa_exoplanet_download
+from Data_management.file_manager import save_catalog
+import numpy as np
+from astropy import units as u
+import exoplanet_table_creator as etc
+from astropy.table import vstack
+
+def extract_base_name(name):
+    return name.split(",")[0]
+
+def duplicate_catalog_with_variations(catalog, star, lq_values, t_sat_values, gamma_values, variation_ids, model_name):
+    mask_star = catalog["hostname"] == star
+    mask_sun = catalog["hostname"] == "Sun"
+
+    star_planets = catalog[mask_star]
+    sun_planets = catalog[mask_sun]
+
+    base_catalog = vstack([sun_planets, star_planets])
+    base_catalog["variation_id"] = "baseline"
+    base_catalog["model_id"] = "baseline_model"
+
+    catalog_extended = [base_catalog]
+
+
+    for n in range(len(variation_ids)):
+        new_tab = star_planets.copy()
+
+        new_tab["variation_id"] = variation_ids[n]
+        new_tab["model_id"] = model_name
+
+        new_tab["Lxuv/Lbol"] = lq_values[n]
+        new_tab["t_sat"] = t_sat_values[n] * u.Gyr
+        new_tab["gamma"] = gamma_values[n]
+
+        catalog_extended.append(new_tab)
+
+    catalog = vstack(catalog_extended)
+    return catalog
+
+def change_lq_and_tsat_for_specific_star(catalog, star, lq, t_sat):
+    mask = catalog["hostname"] == star
+    catalog["t_sat"] = t_sat*u.Gyr
+    catalog["Lxuv/Lbol"][mask] = lq
+    return catalog
+
+
+def main():
+    initials = "AR"
+    catalog = nasa_exoplanet_download.main()
+
+    star = "TRAPPIST-1"
+    star_shortname = "T1"
+    model_name = "Birky 2022"
+
+    suffixes = ["Birky low", "Birky", "Birky high"]
+    specific_lq = [10**-(3.03+0.25), 10**-3.03, 10**-(3.03-0.23)]
+    specific_t_sat = [3.14-1.46, 3.14, 3.14+2.22] # Gyr
+    specific_gamma = [1.17+0.27, 1.17, 1.17-0.28]
+
+    end_time = np.array([0.1, 0.6, 1, 5, 10])  # Gyr
+    output = "fraction"  # "mass" or "fraction"
+    R_xuv = 1.0  # dimensionless ratio >= 1
+    eta = 0.1  # dimensionless heating efficiency
+    protoatmosphere_mass_fraction = 0.01
+
+    description = f"Catalog_comparison_{star}_for_{end_time[0]}-{end_time[-1]}_Gyr_eta-{eta}_Rxuv-{R_xuv}"
+
+    target = "catalog"  # "catalog" or "new"
+
+    variation_ids = ["low", "nom", "high"]
+
+    catalog = duplicate_catalog_with_variations(catalog, star, specific_lq, specific_t_sat, specific_gamma, variation_ids, model_name)
+    catalog = etc.total_loss_calculate_for_catalog(catalog, end_time, R_xuv, eta, protoatmosphere_mass_fraction, target,
+                                               output)
+    catalog.meta["Model"] = model_name
+    catalog.meta["Compared star"] = star
+    catalog.meta["Compared star shortname"] = star_shortname
+    catalog.meta["suffixes"] = ["Birky low", "Birky", "Birky high"]
+    catalog.meta["specific_lq"] = [10 ** -(3.03 + 0.25), 10 ** -3.03, 10 ** -(3.03 - 0.23)]
+    catalog.meta["specific_t_sat"] = [3.14 - 1.46, 3.14, 3.14 + 2.22]  # Gyr
+    catalog.meta["specific_gamma"] = [1.17 + 0.27, 1.17, 1.17 - 0.28]
+
+    save_catalog(catalog, initials, description, filetype="ecsv")
+
+if __name__ == "__main__":
+    main()
